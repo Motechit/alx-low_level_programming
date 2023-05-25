1eb359d1ee40ee0e688ec5b63c44bb6d50dd05df
Motechit <otikomosunmola@gmail.com>
2023-05-25 05:21:13 -0700

    tasks

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - Selects the correct function to perform the operation asked by the user
- * @s: The operator passed as argument
- * Return: A pointer to the function corresponding to the operator given as a parameter.
+ * get_op_func - It selects the correct fxn to perform
+ *               the operation asked by the user
+ * @s: This is the operator passed as arg
+ * Return: This is the ptr to the fxn corresponding
+ *         to the operator given as a parameter.
  */
 
 int (*get_op_func(char *s))(int, int)
